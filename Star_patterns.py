70a57a2e54deb7932172efc04be0d5baa374f917
Bhavish <bhavishmulleshwari@gmail.com>
2026-07-02 21:37:52 +0530

    More problems added

--- Star_patterns.py
+++ Star_patterns.py
@@ -189,7 +189,33 @@ for i in range(1,n+1):
         print(j,end="")
 
     print()
-"""
+
+Pattern 13
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
 n= 5
+start=1
 for i in range(1,n+1):
-    
+    for j in range(i):
+        print(start , end=" ")
+        start+=1
+    print()
+
+
+Pattern 14
+A 
+A B 
+A B C 
+A B C D 
+A B C D E 
+n= 5
+start= ord("A")
+for i in range(1,n+1):
+    for j in range(i):
+        print(chr(start+j) , end=" ")
+    print()
+
+"""
