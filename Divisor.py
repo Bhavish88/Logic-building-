56fa24a4ae545da8415307f7e17e1831cf55dd89
Bhavish <bhavishmulleshwari@gmail.com>
2026-09-02 17:43:14 +0530

    changes

--- Divisor.py
+++ Divisor.py
@@ -1,7 +1,24 @@
-
+"""
 def divisor(x):
     for i in range(1,x+1):
         if x % i == 0:
             print(i)
 x = int(input("Enter a number: "))
 divisor(x) 
+"""
+
+def divisor(x):
+    ans = []
+
+    for i in range(1, int(x ** 0.5) + 1):
+        if x % i == 0:
+            ans.append(i)
+
+            if i != x // i:
+                ans.append(x // i)
+
+    return sorted(ans)
+
+
+x = int(input("Enter a number: "))
+print(divisor(x))
